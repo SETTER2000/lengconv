af4e643b5ce0043bfad293d34a6fb2c907d8d5d7
SETTER <info@lphp.ru>
2018-11-27 16:57:09 +0300

    Added convert to Millimeter

--- conv.go
+++ conv.go
@@ -8,6 +8,10 @@ func CmToM(c Centimeter) Meter {
 	return Meter(c / 100) // преобразование типа Centimeter в Meter
 }
 
+func CmToMm(c Centimeter) Millimeter {
+	return Millimeter(c * 10) // преобразование типа Centimeter в Millimeter
+}
+
 func FtToCm(f Foot) Centimeter {
 	return Centimeter(Centimeter(f) * FootC) // преобразование типа Foot в Centimeter
 }
@@ -16,10 +20,30 @@ func FtToM(f Foot) Meter {
 	return Meter(Meter(f) / Meter(FootM)) // преобразование типа Foot в Meter
 }
 
+func FtToMm(f Foot) Millimeter {
+	return Millimeter(Millimeter(f) * 304.8) // преобразование типа Foot в Millimeter
+}
+
 func MToFt(m Meter) Foot {
 	return Foot(Foot(m) * FootM) // преобразование типа Meter в Foot
 }
 
 func MToCm(m Meter) Centimeter {
 	return Centimeter(Centimeter(m) * 100) // преобразование типа Meter в Centimeter
+}
+
+func MToMm(m Meter) Millimeter {
+	return Millimeter(Millimeter(m) * 1000) // преобразование типа Meter в Millimeter
+}
+
+func MmToCm(m Millimeter) Centimeter {
+	return Centimeter(Centimeter(m) / 10) // преобразование типа Millimeter в Centimeter
+}
+
+func MmToFt(m Millimeter) Foot {
+	return Foot(Foot(m) / 304.8) // преобразование типа Millimeter в Foot
+}
+
+func MmToM(m Millimeter) Meter {
+	return Meter(Meter(m) / 1000) // преобразование типа Millimeter в Meter
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package lengconv
 
 import "fmt"
 
+type Millimeter float64
 type Centimeter float64
 type Meter float64
 type Foot float64
@@ -23,4 +24,8 @@ func (f Foot) String() string {
 
 func (m Meter) String() string {
 	return fmt.Sprintf("%g m", m)
+}
+
+func (m Millimeter) String() string {
+	return fmt.Sprintf("%g mm", m)
 }

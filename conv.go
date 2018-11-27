11a76ea171f991b6d048e569dd5e5f04754184a9
SETTER <info@lphp.ru>
2018-11-27 13:47:57 +0300

    Added Centimeter Foot Meter

--- conv.go
+++ conv.go
@@ -4,6 +4,21 @@ func CmToFt(c Centimeter) Foot {
 	return Foot(c / FootC) // преобразование типа Centimeter в Foot
 }
 
+func CmToM(c Centimeter) Meter {
+	return Meter(c / 100) // преобразование типа Centimeter в Meter
+}
+
 func FtToCm(f Foot) Centimeter {
 	return Centimeter(Centimeter(f) * FootC) // преобразование типа Foot в Centimeter
+}
+
+func FtToM(f Foot) Meter {
+	return Meter(Meter(f) / 3.281) // преобразование типа Foot в Meter
+}
+
+func MToFt(m Meter) Foot {
+	return Foot(Foot(m) * 3.281) // преобразование типа Meter в Foot
+}
+func MToCm(m Meter) Centimeter {
+	return Centimeter(Centimeter(m) * 100) // преобразование типа Foot в Meter
 }
--- main.go
+++ main.go
@@ -1,11 +1,11 @@
-// Преобразования единиц
+// Преобразования единиц длины
+// Сантиметры, Футы, Метры
 package lengconv
 
 import "fmt"
 
 type Centimeter float64
-
-//type Meter float64
+type Meter float64
 type Foot float64
 
 const (
@@ -13,9 +13,13 @@ const (
 )
 
 func (c Centimeter) String() string {
-	return fmt.Sprintf("%g Cm", c)
+	return fmt.Sprintf("%g cm", c)
 }
 
 func (f Foot) String() string {
 	return fmt.Sprintf("%g ft", f)
+}
+
+func (m Meter) String() string {
+	return fmt.Sprintf("%g m", m)
 }

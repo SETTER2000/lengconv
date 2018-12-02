b5cb89aaf9b19198dde4db68deff48dcd710c04d
SETTER <lphp@mail.ru>
2018-12-02 14:06:21 +0300

    Added func inch

--- conv.go
+++ conv.go
@@ -12,6 +12,10 @@ func CmToMm(c Centimeter) Millimeter {
 	return Millimeter(c * 10) // преобразование типа Centimeter в Millimeter
 }
 
+func CmToIn(c Centimeter) Inch {
+	return Inch(c / InchC) // преобразование типа Centimeter в Inch
+}
+
 func FtToCm(f Foot) Centimeter {
 	return Centimeter(Centimeter(f) * FootC) // преобразование типа Foot в Centimeter
 }
@@ -22,6 +26,10 @@ func FtToM(f Foot) Meter {
 
 func FtToMm(f Foot) Millimeter {
 	return Millimeter(Millimeter(f) * FootMM) // преобразование типа Foot в Millimeter
+}
+
+func FtToIn(f Foot) Inch {
+	return Inch(Inch(f) * 12) // преобразование типа Foot в Inch
 }
 
 func MToFt(m Meter) Foot {
@@ -36,6 +44,10 @@ func MToMm(m Meter) Millimeter {
 	return Millimeter(Millimeter(m) * 1000) // преобразование типа Meter в Millimeter
 }
 
+func MToIn(m Meter) Inch {
+	return Inch(Inch(m) * 39.37) // преобразование типа Meter в Inch
+}
+
 func MmToCm(m Millimeter) Centimeter {
 	return Centimeter(Centimeter(m) / 10) // преобразование типа Millimeter в Centimeter
 }
@@ -46,4 +58,24 @@ func MmToFt(m Millimeter) Foot {
 
 func MmToM(m Millimeter) Meter {
 	return Meter(Meter(m) / 1000) // преобразование типа Millimeter в Meter
+}
+
+func MmToIn(m Millimeter) Inch {
+	return Inch(m / InchMm) // преобразование типа Millimeter в Inch
+}
+
+func InToMm(i Inch) Millimeter {
+	return Millimeter(Millimeter(i) + InchMm) // преобразование типа Inch в Millimeter
+}
+
+func InToCm(i Inch) Centimeter {
+	return Centimeter(Centimeter(i) * InchC) // преобразование типа Inch в Centimeter
+}
+
+func InToM(i Inch) Meter {
+	return Meter(Meter(i) * InchM) // преобразование типа Inch в Meter
+}
+
+func InToF(i Inch) Foot {
+	return Foot(Foot(i) * InchF) // преобразование типа Inch в Foot
 }
--- main.go
+++ main.go
@@ -7,11 +7,16 @@ type Millimeter float64
 type Centimeter float64
 type Meter float64
 type Foot float64
+type Inch float64
 
 const (
 	FootC  Centimeter = 30.48
 	FootM  Foot       = 3.281
 	FootMM Millimeter = 304.8
+	InchC  Centimeter = 2.54
+	InchMm Millimeter = 25.4
+	InchM  Meter      = 0.0254
+	InchF  Foot       = 0.0833333
 )
 
 func (c Centimeter) String() string {
@@ -28,4 +33,8 @@ func (m Meter) String() string {
 
 func (m Millimeter) String() string {
 	return fmt.Sprintf("%g mm", m)
+}
+
+func (i Inch) String() string {
+	return fmt.Sprintf("%g in", i)
 }
